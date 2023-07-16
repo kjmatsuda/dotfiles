468453ad9761cde89aa33d09913e026ece655a13
Koji Matsuda <kjmatsuda1985@gmail.com>
2023-07-16 17:53:07 +0900

    インストール用スクリプトのバックアップフォルダ名修正と、ln コマンド実行条件修正

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKUPDIR="${HOME}/backup/dotfiles/$(date +"%Y%m%d%I%M%S")"
+BACKUPDIR="${HOME}/backup/dotfiles/$(date +"%Y%m%d%H%M%S")"
 
 if [ ! -d "${BACKUPDIR}" ]; then
     echo "mkdir -p ${BACKUPDIR}"
@@ -15,11 +15,13 @@ do
         echo "mkdir -p ${BACKUPDIR}/$(dirname ${file})"
         mkdir -p ${BACKUPDIR}/$(dirname ${file})
     fi
-    cp -p ${HOMEDOTFILE} ${BACKUPDIR}/${file}
 
-    if [ -e "${BACKUPDIR}/${file}" ]; then
-        # バックアップが作成できたら、シンボリックリンク作成
-        echo "${BACKUPDIR}/${file} exists"
+    if [ -e "${HOMEDOTFILE}" ]; then
+        cp -p ${HOMEDOTFILE} ${BACKUPDIR}/${file}
+    fi
+
+    if [ -e "${BACKUPDIR}/${file}" ] || [ ! -e "${HOMEDOTFILE}" ]; then
+        echo "ln -sf ${CURRENTDOTFILE} ${HOMEDOTFILE}"
         ln -sf ${CURRENTDOTFILE} ${HOMEDOTFILE}
     fi
 done
